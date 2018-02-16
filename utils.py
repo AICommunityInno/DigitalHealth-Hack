4d390c97208c1dd9514408c99e21d7a538639f90
Vlad Vinogradov <vladt9@yandex.ru>
2018-02-17 01:30:52 +0300

    Added method for picking the most popular diagnoses

---
+++ utils.py
@@ -0,0 +1,14 @@
+def get_most_popular_diagnoses(data, percent=95):
+    diagnoses = list(np.sort(np.unique(data['Код_диагноза'])))
+    diag_to_num = dict(zip(diagnoses, range(len(diagnoses))))
+    num_to_diag = dict(zip(range(len(diagnoses)), diagnoses))
+    
+    data = data.copy()
+    data['diag_idx'] = data['Код_диагноза'].apply(lambda code: diag_to_num[code])
+    diag_idxs, counts = np.unique(data['diag_idx'], return_counts=True)
+    sorted_idxs = np.argsort(counts)[::-1]
+    
+    popular_idxs = diag_idxs[counts >= np.percentile(counts, q=percent)]
+    popular_diagnoses = list(map(lambda idx: num_to_diag[idx], popular_idxs))
+    
+    return popular_diagnoses
