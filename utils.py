90d45e400501e8971da6a18a49772d560c2277d3
Vlad Vinogradov <vladt9@yandex.ru>
2018-02-17 14:49:18 +0300

    Fixed most popular diagnoses picking method

--- utils.py
+++ utils.py
@@ -1,12 +1,6 @@
-def get_most_popular_diagnoses(data, percent=95):
-    cumsums = np.cumsum(train_df.Код_диагноза.value_counts())
-    most_classes = data.Код_диагноза.value_counts()[cumsums <= percent * len(train_df)]
-    return list(most_classes.index)
+import numpy as np
 
-def get_doctor(string):
-    string = string.replace(',', ' ')
-    splitted_str = string.split('врача-')
-    if len(splitted_str) > 1:
-        return splitted_str[1].split(' ')[0]
-    else:
-        return 'нет'
+def get_most_popular_diagnoses(data, percent=.95):
+    cumsums = np.cumsum(data.Код_диагноза.value_counts())
+    most_classes = data.Код_диагноза.value_counts()[cumsums <= percent * len(data)]
+    return list(most_classes.index)
