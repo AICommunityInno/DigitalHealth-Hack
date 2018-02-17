a610ce477bb8ccaa9224fe9d2afef06776bb22ed
root <root@hack2018-003>
2018-02-17 08:12:03 -0600

    Fixed utils

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def get_most_popular_diagnoses(data, percent=.95):
-    cumsums = np.cumsum(data.Код_диагноза.value_counts())
-    most_classes = data.Код_диагноза.value_counts()[cumsums <= percent * len(data)]
+    cumsums = np.cumsum(data.value_counts())
+    most_classes = data.value_counts()[cumsums <= percent * len(data)]
     return list(most_classes.index)
